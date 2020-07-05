6ab60e5a1fef7250ae1409454baeabdca015e7ed
Sadiq Noor <--replace-all>
2020-07-06 01:50:48 +0400

    create database and seed for menu and display menu by vue js

--- resources/js/modules/menu/MenuContainer.vue
+++ resources/js/modules/menu/MenuContainer.vue
@@ -1,11 +1,75 @@
 <template>
-	<div class="wrapper">
-		<p v-for="item in items"> display menu items</p>
-	</div>
+    <div class="wrapper manu__container">
+        <div class="row">
+            <div class="col-md-8">
+                <card-component>
+
+                    <template slot='title'>My Menu Items</template>
+                    
+                    <template slot='main'>
+                        <div class="section">
+                            <Multiselect 
+                                :options="categories"
+                                v-model='menu'
+                                >
+                            </multiselect>
+                        </div>
+
+                        <menu-groups :items="currentMenuItems" ></menu-groups>
+                    </template>
+
+
+
+                </card-component>
+
+            </div>  
+            <div class="col-md-4">
+                
+                <div class="card">  
+                    <card-component>
+                        <template slot='title'>Add menu items</template>
+                        <template slot='main'>Form come here</template>
+
+                    </card-component>
+                </div>
+
+            </div>  
+        </div>
+    </div>
 </template>
+
 <script>
-	export default{
-		props: ['items'],
-		
-	}
+Vue.component('card-component', require('../../components/Card.vue').default);
+import Multiselect from 'vue-multiselect';
+import MenuGroups from "./MenuGroups.vue";
+    export default {
+        props: ['items'],
+        components: {
+          
+            Multiselect, MenuGroups
+        
+        },
+
+        created() {
+            _.forEach(this.items, (item, key) => {
+                this.categories.push(key);
+            });
+
+            this.menu = this.categories[0]
+        },
+
+        data() {
+            return {
+                categories: [],
+                menu: ''
+            }
+        },
+
+        computed: {
+            currentMenuItems() {
+                return this.items[this.menu];
+            }
+        }
+        
+    }
 </script>
---
+++ resources/js/modules/menu/MenuGroups.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="section">
+        <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>Name & Description</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, key) in items" :key="key">
+                    <td>
+                        <strong v-text="item.name"></strong>
+                        <p v-text="item.description"></p>
+                    </td>
+                    <td><strong v-text="item.price"></strong></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+	export default {
+		props: ['items']
+	}
+</script>

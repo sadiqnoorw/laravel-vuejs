b29e9f21255f611755553ba25343d4e49265e6d2
Sadiq Noor <--replace-all>
2020-07-19 01:28:40 +0400

    MunchExpress 13 Vuejs conditional render to show Restos and Add button

---
+++ resources/js/modules/restos/RestoGroup.vue
@@ -0,0 +1,45 @@
+<template>
+
+	<div class="resto-group-wrapper mb-5">
+		<div class="row">
+			<div class="col-md-4" v-for="resto in restos" :key="resto.id">
+				
+				<card-component>
+					<template slot="title"> {{resto.name}} </template>	
+					<template slot="main"> {{resto.location}} </template>	
+				</card-component>
+				
+			</div>
+
+			<div class="col-md-4" v-if="showAddForm">
+				<card-component>
+					<template slot="title"> Add new Restaurant </template>	
+					<template slot="main"> + </template>		
+				</card-component>				
+			</div>
+	
+		</div>	
+	</div>
+
+</template>
+
+<script>
+	export default {
+		props: ['restos'],
+
+		created() {
+			console.log("this.restos.length", this.restos.length);
+		},
+
+		computed: {
+			showAddForm(){
+				return (this.restos.length < 5) ? true : false ;		
+			}
+		},
+
+		data() {
+			return {}
+		}
+
+	}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="container">
+
+    <restos-group :restos="{{json_encode($restos)}}"></restos-group>
+
     <menu-container :items="{{json_encode($menus)}}" resto-id="{{$restoId}}"></menu-container>
 </div>
 

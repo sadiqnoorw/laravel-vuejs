cb6b5333a94142a1ed461fcae93d00379836961d
Sadiq Noor <--replace-all>
2020-08-01 06:28:34 +0400

    form for Orders also look at computed properties

--- resources/js/modules/orders/OrderMenuItems.vue
+++ resources/js/modules/orders/OrderMenuItems.vue
@@ -9,7 +9,7 @@
 					<small>{{ item.category.name }}</small>
 				</div>			
 				<div class="col-md-2">
-					<button class="btn btn-primary btn-sm">Add</button>
+					<button class="btn btn-primary btn-sm" @click="handleAddButton(item)">Add</button>
 				</div>			
 			</div>
 		</li>
@@ -17,7 +17,13 @@
 </template>
 <script>
 	export default {
-		props: ['items']
+		props: ['items'],
+
+		methods: {
+			handleAddButton(item) {
+				this.$emit('menuItemAdded', item);
+			}
+		}
 
 	}
 </script>
